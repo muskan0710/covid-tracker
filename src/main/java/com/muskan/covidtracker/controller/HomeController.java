661832425e3af0833fe9473a846e20925f460087
muskan0710 <muskansharma0710@gmail.com>
2022-05-03 04:11:12 +0530

    Covid tracker

---
+++ src/main/java/com/muskan/covidtracker/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.muskan.covidtracker.controller;
+
+import com.muskan.covidtracker.entity.LocationStats;
+import com.muskan.covidtracker.services.CovidDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CovidDataService covidDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        List<LocationStats> allStats = covidDataService.getAllStats();
+        int totalReportedCases = allStats.stream()
+                .mapToInt(LocationStats::getLatestTotalCases)
+                .sum();
+        int totalNewCases = allStats.stream()
+                .mapToInt(LocationStats::getDiffFromPrevDay)
+                .sum();
+
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+}
